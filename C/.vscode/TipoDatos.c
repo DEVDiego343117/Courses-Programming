e553ba13a3d5a2fa10ade9f7fa0ecd8568822474
Diego Samuel Arias Diaz <devdiego343@outlook.es>
2025-01-22 18:11:27 -0600

    data types

--- C/.vscode/TipoDatos.c
+++ C/.vscode/TipoDatos.c
@@ -7,11 +7,15 @@ int main (){
     float pi = 3.1415926;
     double pi2 = 3.141592653589793;
     bool answer = true;
+    unsigned char y = 255;
+    int number =2025;
     
     printf("%c\n" ,letter);
     printf("%f\n" ,pi);
     printf("%lf\n" ,pi2);
     printf("%d\n" ,answer);
+    printf("%d\n" ,y);
+    printf("%i" ,number);
 
     return 0;
 }
