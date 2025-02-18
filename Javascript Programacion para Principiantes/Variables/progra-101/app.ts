685014c4530fc55848035248827aa24e0d7de9d9
Diego Samuel Arias Diaz <devdiego343@outlook.es>
2025-02-18 16:16:29 -0600

    array object

--- Javascript Programacion para Principiantes/Variables/progra-101/app.ts
+++ Javascript Programacion para Principiantes/Variables/progra-101/app.ts
@@ -1,16 +1,1 @@
-let person = {
-    //Objeto literal
-    name: "Diego",
-    age: 18,
-    isActive: true,
-    hobbies: ["Basketball", "Music", "Videogames"],
-
-    //Programacion metodo
-    toString(printAll:boolean) {
-        let objectString = this.name + ' '+ this.age + " " + this.hobbies + " " + this.isActive;
-        console.log(objectString);
-    }
-}
-
-// console.log(person.name.toUpperCase());
-person.toString(true);
+console.log("ola")
