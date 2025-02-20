09861c2e8f49cf82cf77d17a89ed2a29617d24b1
Diego Samuel Arias Diaz <devdiego343@outlook.es>
2025-02-20 16:01:30 -0600

    class

--- Javascript Programacion para Principiantes/Variables/progra-101/app.ts
+++ Javascript Programacion para Principiantes/Variables/progra-101/app.ts
@@ -1,1 +1,40 @@
-console.log("ola")
+class Car {
+    brand: string;
+    doors: number;
+    gearbox: string;
+    speed: number;
+    fuelTank: number;
+    isRunning: boolean;
+
+    constructor() {
+        this.brand = "Camaro";
+        this.doors = 4;
+        this.gearbox = "Automatico";
+        this.speed = 150;
+        this.fuelTank = 50;
+        this.isRunning = false;
+
+    }
+
+    turnOn() {
+        if (this.isRunning){
+           console.log("El carro ya estaba encendido");
+           return;
+        }
+
+        if(this.fuelTank <=0){
+            console.log("El carro no tiene combustible");
+            return;
+        }
+
+        this.isRunning = true;
+        console.log("El carro esta encendido");
+        
+    }
+}
+
+let camaro = new Car();
+
+console.log(camaro);
+camaro.turnOn();
+camaro.turnOn();
